028dc71f02d8495963f6f8fc457f8b000001a591
Valentin Yakushev <valentin7petr@gmail.com>
2025-03-24 20:06:26 +0300

    validation for SignupRequest

--- src/main/java/ru/yakaska/tasktrackerapi/payload/request/SignupRequest.java
+++ src/main/java/ru/yakaska/tasktrackerapi/payload/request/SignupRequest.java
@@ -1,16 +1,21 @@
 package ru.yakaska.tasktrackerapi.payload.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
 public class SignupRequest {
 
+    @Email
+    @NotBlank
     private final String email;
 
+    @NotBlank
     private final String password;
 
-    @Pattern(regexp = "ADMIN|USER", message = "Role must be either ADMIN or USER")
+    @Pattern(regexp = "ADMIN|USER", message = "UserRole must be either ADMIN or USER")
     private String role;
 
     public SignupRequest(String email, String password) {
