058090e3aa72ce036d68220ced7b39df11f511e3
Valentin Yakushev <valentin7petr@gmail.com>
2025-03-25 23:37:08 +0300

    Так нельзя, но начальную миграцию поменял

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,7 +1,7 @@
 -- Создание таблицы пользователей
 CREATE TABLE users
 (
-    id    SERIAL PRIMARY KEY,
+    id         SERIAL PRIMARY KEY,
     email      VARCHAR(255) UNIQUE NOT NULL,
     password   VARCHAR(255)        NOT NULL,
     role       VARCHAR(50)         NOT NULL CHECK (role IN ('ADMIN', 'USER')),
@@ -11,7 +11,7 @@ CREATE TABLE users
 -- Создание таблицы задач
 CREATE TABLE tasks
 (
-    id     SERIAL PRIMARY KEY,
+    id          SERIAL PRIMARY KEY,
     title       VARCHAR(255) NOT NULL,
     description TEXT,
     status      VARCHAR(50)  NOT NULL CHECK (status IN ('PENDING', 'IN_PROGRESS', 'COMPLETED')),
@@ -19,7 +19,7 @@ CREATE TABLE tasks
     author_id   INT          NOT NULL,
     assignee_id INT,
     created_at  TIMESTAMP DEFAULT NOW(),
-    updated_at  TIMESTAMP,
+    updated_at  TIMESTAMP DEFAULT NOW(),
     FOREIGN KEY (author_id) REFERENCES users (id) ON DELETE CASCADE,
     FOREIGN KEY (assignee_id) REFERENCES users (id) ON DELETE SET NULL
 );
@@ -27,7 +27,7 @@ CREATE TABLE tasks
 -- Создание таблицы комментариев
 CREATE TABLE comments
 (
-    id SERIAL PRIMARY KEY,
+    id         SERIAL PRIMARY KEY,
     task_id    INT  NOT NULL,
     user_id    INT  NOT NULL,
     content    TEXT NOT NULL,
@@ -41,6 +41,19 @@ CREATE INDEX idx_tasks_author_id ON tasks (author_id);
 CREATE INDEX idx_tasks_assignee_id ON tasks (assignee_id);
 CREATE INDEX idx_comments_task_id ON comments (task_id);
 
--- Добавление администратора по умолчанию (пароль нужно хешировать)
+-- Тестовые данные
+
 INSERT INTO users (email, password, role)
-VALUES ('admin@example.com', '$2a$10$somehashedpassword', 'ADMIN');
+VALUES ('valentin7petr@gmail.com', '$2y$10$YtmxI1XVdZVf3B4eKqY/o.CGOXSlXqrofgCnjqYR.28ncuCgeuHpu', 'ADMIN');
+
+INSERT INTO tasks (title, description, status, priority, author_id, assignee_id, created_at, updated_at)
+VALUES ('Task 1', 'Description for Task 1', 'PENDING', 'HIGH', 1, 1, NOW(), NOW()),
+       ('Task 2', 'Description for Task 2', 'PENDING', 'MEDIUM', 1, 1, NOW(), NOW()),
+       ('Task 3', 'Description for Task 3', 'IN_PROGRESS', 'LOW', 1, 1, NOW(), NOW()),
+       ('Task 4', 'Description for Task 4', 'IN_PROGRESS', 'HIGH', 1, 1, NOW(), NOW()),
+       ('Task 5', 'Description for Task 5', 'COMPLETED', 'MEDIUM', 1, 1, NOW(), NOW()),
+       ('Task 6', 'Description for Task 6', 'PENDING', 'HIGH', 1, 1, NOW(), NOW()),
+       ('Task 7', 'Description for Task 7', 'COMPLETED', 'LOW', 1, 1, NOW(), NOW()),
+       ('Task 8', 'Description for Task 8', 'PENDING', 'MEDIUM', 1, 1, NOW(), NOW()),
+       ('Task 9', 'Description for Task 9', 'IN_PROGRESS', 'HIGH', 1, 1, NOW(), NOW()),
+       ('Task 10', 'Description for Task 10', 'IN_PROGRESS', 'MEDIUM', 1, 1, NOW(), NOW());
